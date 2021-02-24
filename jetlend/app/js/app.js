32d524dfbc857aca9b5b51728899825c9ea6b966
mrdeviantart <yuriikovalchukk@gmail.com>
2021-02-24 22:04:34 +0200

    header fixed + burger fixed done

--- jetlend/app/js/app.js
+++ jetlend/app/js/app.js
@@ -8,8 +8,47 @@ const burgerNav = $(".burger-nav");
 
 burgerBtn.on("click", function (e) {
     e.preventDefault();
+    // e.stopPropagation();
     $(this).toggleClass("burger-btn-active");
     burgerMenu.toggleClass("burger-menu-active");
     burgerNav.toggleClass("burger-nav-active");
-})
+});
 
+$(document).on("click", function (e) {
+    if (!burgerMenu.is(e.target) && burgerMenu.has(e.target).length === 0 && !burgerBtn.is(e.target) && burgerBtn.has(e.target).length === 0) {
+        burgerBtn.removeClass("burger-btn-active")
+        burgerMenu.removeClass("burger-menu-active");
+        burgerNav.removeClass("burger-nav-active");
+
+        console.log("hi");
+    }
+});
+
+
+
+//--------------------------------------------------------------
+// Header fixed
+//--------------------------------------------------------------
+
+const header = $(".header");
+const headerHeight = $(".header").innerHeight();
+const main = $(".main");
+let scrollOffset = $(window).scrollTop();
+
+checkScroll(scrollOffset);
+
+$(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
+    checkScroll(scrollOffset);
+});
+
+function checkScroll(scrollOffset) {
+    if (scrollOffset >= headerHeight) {
+        header.addClass("header--fixed");
+        main.addClass("main-active");
+    } else if (scrollOffset === 0) {
+        header.removeClass("header--fixed");
+        main.removeClass("main-active");
+    }
+
+}
