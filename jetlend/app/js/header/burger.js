3f440de73680e11333e9bf3856a6d255e002c0b7
mrdeviantart <yuriikovalchukk@gmail.com>
2021-03-07 02:10:52 +0200

    header redone

--- jetlend/app/js/header/burger.js
+++ jetlend/app/js/header/burger.js
@@ -3,21 +3,46 @@
 //--------------------------------------------------------------
 
 const burgerBtn = $(".burger-btn");
-const burgerMenu = $(".burger-menu");
-const burgerNav = $(".burger-nav");
+const menu = $(".menu");
+const body = $("body");
 
 burgerBtn.on("click", function (e) {
     e.preventDefault();
-    // e.stopPropagation();
-    $(this).toggleClass("burger-btn-active");
-    burgerMenu.toggleClass("burger-menu-active");
-    burgerNav.toggleClass("burger-nav-active");
+    body.toggleClass("show-menu");
 });
 
 $(document).on("click", function (e) {
-    if (!burgerMenu.is(e.target) && burgerMenu.has(e.target).length === 0 && !burgerBtn.is(e.target) && burgerBtn.has(e.target).length === 0) {
-        burgerBtn.removeClass("burger-btn-active")
-        burgerMenu.removeClass("burger-menu-active");
-        burgerNav.removeClass("burger-nav-active");
+    if (!menu.is(e.target) && menu.has(e.target).length === 0 && !burgerBtn.is(e.target) && burgerBtn.has(e.target).length === 0) {
+        body.removeClass("show-menu");
     }
 });
+
+const navLink = document.querySelectorAll(".nav-link");
+const navBtn = document.querySelector(".nav-btn");
+// console.log(navBtn);
+// console.log(navLink);
+
+const menuArr = [...navLink, navBtn];
+// console.log(menuArr);
+
+
+
+document.querySelector(".burger-btn").addEventListener("click", function () {
+    if (document.getElementById("body").classList.contains("show-menu")) {
+        function animateLinks(i) {
+            menuArr[i].classList.add("active");
+            if (i < menuArr.length - 1) {
+                setTimeout(animateLinks, 30, ++i);
+            }
+        }
+
+        setTimeout(animateLinks, 300, 0);
+    } else {
+        for (let i = 0; i < menuArr.length; i++) {
+            menuArr[i].classList.remove("active");
+        }
+    }
+});
+
+
+
